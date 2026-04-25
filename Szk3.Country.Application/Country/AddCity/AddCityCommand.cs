a58fc198158917ffeb1618862a800dcb910669c8
Filip Furgał <67739@student.wsiz.edu.pl>
2026-04-25 13:58:11 +0200

    new lab, new stuff

---
+++ Szk3.Country.Application/Country/AddCity/AddCityCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szk3.Country.Application.Country.AddCity
+{
+    public sealed record AddCityCommand(int CountryId, string Name, bool IsActive = true): IRequest<int>
+    {
+    }
+}
